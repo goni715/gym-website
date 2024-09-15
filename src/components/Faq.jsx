c183c7d2fd337e56ebe43257bc852151c13e1f89
goni715 <halesgoni715149123@gmail.com>
2024-09-15 20:47:46 +0600

    faq correction

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,32 +2,40 @@ import { faq } from "../data/data";
 import Accordion from "./Accordion";
 
 const Faq = () => {
-    const { icon, title, accordions} = faq;
+  const { icon, title, accordions } = faq;
 
-    return (
-        <>
-            <section className="pb-20 md:pb-28 lg:pb-[180px] pt-[280px] mb-[80px] lg:mb-0">
-                <div className="max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:min-h-[1160px] lg:pt-6">
-                    {/* section title*/}
-                    <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="200" className="flex items-center mb-[50px] lg:mb-20 justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0">
-                        <img className="lg:hidden" src={icon} alt="faq_icon" />
-                        <h2 className="text-3xl leading-10 lg:text-5xl lg:leading-[50px] font-extrabold differ flex -space-x-4 lg:mt-[100px]">
-                            {title}
-                            <span className="text-primary-200">.</span>
-                        </h2>
-                    </div>
-                    {/* accordion list */}
-                    <div className="flex flex-col gap-y-4 px-4" data-aos="fade-up" data-aos-offset="300" data-aos-delay="200">
-                    {accordions?.map((accordion, index)=>{
-                        return (
-                           <Accordion key={index} accordion={accordion}/>
-                        )
-                    })}
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="pb-20 md:pb-28 lg:pb-[180px] pt-[100px] lg:pt-[180px] mb-[80px] lg:mb-0">
+        <div className="max-w-[768px] mx-auto lg:bg-faq bg-no-repeat bg-custom bg-center lg:min-h-[1160px] lg:pt-6">
+          {/* section title*/}
+          <div
+            data-aos="fade-up"
+            data-aos-offset="200"
+            data-aos-delay="200"
+            className="flex items-center mb-[50px] lg:mb-20 justify-start lg:justify-center -space-x-4 lg:max-w-[540px] mx-auto px-4 lg:px-0"
+          >
+            <img className="lg:hidden" src={icon} alt="faq_icon" />
+            <h2 className="text-3xl leading-10 lg:text-5xl lg:leading-[50px] font-extrabold differ flex -space-x-4 lg:mt-[100px]">
+              {title}
+              <span className="text-primary-200">.</span>
+            </h2>
+          </div>
+          {/* accordion list */}
+          <div
+            className="flex flex-col gap-y-4 px-4"
+            data-aos="fade-up"
+            data-aos-offset="300"
+            data-aos-delay="200"
+          >
+            {accordions?.map((accordion, index) => {
+              return <Accordion key={index} accordion={accordion} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Faq;
